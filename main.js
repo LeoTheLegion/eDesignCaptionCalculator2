d28359f4625f493a02a4bce62ef9405d137e5b37
unknown <celstudio@BOC04LT-66D108.irm.ad.fau.edu>
2018-07-12 15:42:51 -0400

    Fixed two runs bug and added a cleaner look

--- main.js
+++ main.js
@@ -12,13 +12,16 @@ const unzip = require('unzipper')
   
   function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({
+		width: 550, 
+		height: 350})
   
     // and load the index.html of the app.
     win.loadFile('index.html')
-  
+  	win.setResizable(false)
+	win.setMenu(null)
     // Open the DevTools.
-   //win.webContents.openDevTools()
+  // win.webContents.openDevTools()
   
     // Emitted when the window is closed.
     win.on('closed', () => {
@@ -59,7 +62,7 @@ const unzip = require('unzipper')
 	console.log( "Main:" + file );
 		
 	var foundcount = 0;
-	var name = file.replace(/^.*[\\\/]/, '').slice(0,-4);
+	var name = (file.replace(/^.*[\\\/]/, '').slice(0,-4)).replace(/\s/g, "");;
 	fs.createReadStream(file)
 	  .pipe(unzip.Parse())
 	  .on('entry', function (entry) {
